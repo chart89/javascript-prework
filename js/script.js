2602acb5304eade482b46d5d58a4c8001371dbe8
Darek Biernat <kaiju83@outlook.com>
2023-03-03 22:49:30 +0100

    Add variables

--- js/script.js
+++ js/script.js
@@ -7,3 +7,9 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+let playerMove = `papier`;
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let computerMove = `kamień`;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
