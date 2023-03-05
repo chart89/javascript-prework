93561909d378361ee161510f726209e8ffcf8267
Darek Biernat <kaiju83@outlook.com>
2023-03-05 20:50:19 +0100

    Add player move javascript code

--- js/script.js
+++ js/script.js
@@ -8,8 +8,18 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let playerMove = `papier`;
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-let computerMove = `kamień`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}else if(playerInput == '2'){
+	playerMove = 'papier';
+}else if(playerInput == '3'){
+	playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
