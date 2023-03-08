3abfbf411a0976a23ac42569a27d3735df52bae5
Darek Biernat <kaiju83@outlook.com>
2023-03-08 19:06:29 +0100

    Remove unnecessary code

--- js/script.js
+++ js/script.js
@@ -47,31 +47,12 @@ function playGame(argplayerChoice){
 
 	let computerMove = getMoveName(randomNumber);
 
-	/*
-	if(randomNumber == 1){
-	computerMove = 'kamień';
-	}else if(randomNumber == 2){
-		computerMove = 'papier';
-	}else if(randomNumber = '3'){
-		computerMove = 'nożyce';
-	}
-	*/
-
-	/* let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'); */
 
 	console.log('Gracz wpisał: ' + playerInput);
 
 	let playerMove = getMoveName(playerInput);
 
-	/*
-	if(playerInput == '1'){
-	playerMove = 'kamień';
-	}else if(playerInput == '2'){
-		playerMove = 'papier';
-	}else if(playerInput == '3'){
-		playerMove = 'nożyce';
-	}
-	*/
+	
 	argPlayerMove = getMoveName(playerInput);
 	argComputerMove = getMoveName(randomNumber);
 	displayResult(argComputerMove, argPlayerMove);
